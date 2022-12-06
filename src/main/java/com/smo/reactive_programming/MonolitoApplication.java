904937176451605baaf0a3e0f5c2b34bdd4ddb84
Sebastian Medina Ochoa <semedin@bancolombia.com.co>
2022-12-06 10:30:47 -0500

    Cambio de estructura

---
+++ src/main/java/com/smo/reactive_programming/MonolitoApplication.java
@@ -0,0 +1,13 @@
+package com.smo.reactive_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitoApplication.class, args);
+	}
+
+}
