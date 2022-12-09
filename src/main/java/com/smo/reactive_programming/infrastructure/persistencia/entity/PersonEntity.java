eb40ecef50d77329e723c9560bcef3997edf2e61
Sebastian Medina Ochoa <semedin@bancolombia.com.co>
2022-12-09 10:56:26 -0500

    Configuración persistencia

--- src/main/java/com/smo/reactive_programming/infrastructure/persistencia/entity/PersonEntity.java
+++ src/main/java/com/smo/reactive_programming/infrastructure/persistencia/entity/PersonEntity.java
@@ -1,13 +1,17 @@
 package com.smo.reactive_programming.infrastructure.persistencia.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "PERSON")
+@Data
+@NoArgsConstructor
+@Document(collection = "PERSONS")
 public class PersonEntity {
 
     @Id
-    private Long personId;
+    private String personId;
     private String clientName;
     private String clientLastName;
     private String clientYear;
