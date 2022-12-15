7e8511729d8306657ca743fc799ac80d36afd0c8
Sebastian Medina Ochoa <semedin@bancolombia.com.co>
2022-12-15 16:32:56 -0500

    Creación del endpointReatFlux

--- src/main/java/com/smo/reactive_programming/infrastructure/rest/controller/EndpointGetPersons.java
+++ src/main/java/com/smo/reactive_programming/infrastructure/rest/controller/EndpointGetPersons.java
@@ -4,8 +4,6 @@ import com.smo.reactive_programming.application.gateways.GetPersonsInt;
 import com.smo.reactive_programming.domain.answer.AnswerData;
 import com.smo.reactive_programming.domain.answer.DataResponse;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +20,10 @@ public class EndpointGetPersons {
 
     private final GetPersonsInt getPersonsInt;
 
-    private static final Logger log = LoggerFactory.getLogger(EndpointGetPersons.class);
-
     @GetMapping(value = "/getPersons")
     public Mono<ResponseEntity<AnswerData>> getPersons() {
         return Mono.just(getPersonsInt.getPersons()).flatMap(person -> Mono.just(DataResponse.builder().message(
-                "Sastifactorio").data(Optional.of(person)).build())
+                        "Sastifactorio").data(Optional.of(person)).build())
                 .flatMap(dataResponse -> Mono.just(new AnswerData(HttpStatus.OK, dataResponse))
                         .flatMap(answerData -> Mono.just(new ResponseEntity<>(answerData,
                                 answerData.getStatus())))));
